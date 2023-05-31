62ab9ed663bf3bc8a9b707c99ac6157be82f758f
Sakonya <sakonya@gmail.com>
2023-05-31 12:06:40 +0600

    About us
    Created the "About Us" page

---
+++ AboutUs/ab.js
@@ -0,0 +1,35 @@
+var backgroundMusic = document.getElementById("backgroundMusic");
+var soundIcon = document.getElementById("soundIcon");
+var isMuted = false;
+
+soundIcon.addEventListener("click", function() {
+  if (isMuted) {
+    backgroundMusic.play();
+    soundIcon.classList.remove("muted");
+    isMuted = false;
+  } else {
+    backgroundMusic.pause();
+    soundIcon.classList.add("muted");
+    isMuted = true;
+  }
+});
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+
+
