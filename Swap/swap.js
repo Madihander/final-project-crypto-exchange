86e0ed96334038b7f92e75632055b78c70aee7f5
Madihander <79453431+Madihander@users.noreply.github.com>
2023-06-03 12:19:43 +0600

    Update swap.js

--- Swap/swap.js
+++ Swap/swap.js
@@ -26,3 +26,47 @@ function updateSecondSelect() {
         secondSelect.value = "INT";
     }
 }
+
+function requestAccount() {
+    console.log('Requesting Account...')
+    // Проверяем есть ли ethereum клиент
+    if (window.ethereum) {
+        console.log('detected')
+        ethereum.request({method: 'eth_requestAccounts'})
+            .then(function (accounts) {
+                if (accounts.length > 0) {
+                    console.log("Выбран аккаунт: ", accounts[0]);
+                    account = accounts[0];
+                    document.getElementById('addressWallet').textContent = account;
+                } else {
+                    console.log("Аккаунт не выбран");
+                }
+            })
+            .catch(function (error) {
+                // Обработка ошибки при запросе аккаунтов
+                console.error(error);
+            });
+    }
+}
+
+$(document).ready(function() {
+    $('#swapButton').click(function() {
+        $('#swapButton').text('Swapping!');
+    });
+
+    $('#swapButton').mouseover(function() {
+        console.log('Swap your Tokens')
+    });
+
+    $('#swap-value-start').keypress(function (){
+        $('#aggetation').text("!!! Swap more Tokens !!!")
+    })
+
+    $('#swapForm').submit(function(event) {
+        event.preventDefault(); // Отменяет перезагрузку страницы
+        // Ваш код обработки формы здесь
+    });
+
+});
+
+
